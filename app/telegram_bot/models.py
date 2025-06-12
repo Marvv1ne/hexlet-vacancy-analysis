2ab550a04d4699adc3643578957224126fdd3791
Marwin <gorn89@inbox.ru>
2025-06-12 14:51:32 +0400

    feat: add simple model for telgram user

--- app/telegram_bot/models.py
+++ app/telegram_bot/models.py
@@ -1,3 +1,8 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
+class TgUser(models.Model):
+    username = models.CharField(max_length=50)
+    filters = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now=True)
