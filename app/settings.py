a736d959fb1f651994c74e73e7ddf3abfe9e2b94
Marwin <gorn89@inbox.ru>
2025-06-29 16:35:21 +0400

    feat: add django_celery_beat into installed apps and celery settings

--- app/settings.py
+++ app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_beat',
     'app.services.hh.hh_parser',
     'app.telegram_bot',
 ]
@@ -144,3 +145,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #Telegram bot settings
 
 TELEGRAM_BOT_TOKEN = os.getenv('TOKEN')
+
+# Celery settings
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
