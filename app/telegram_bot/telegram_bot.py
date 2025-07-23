85c4152ab14fbec487a8ce221a8dc705a6c8375a
Marvv1ne <gorn89@inbox.ru>
2025-07-23 22:25:46 +0400

    fix: delete unnessesary imports and bot commands

--- app/telegram_bot/telegram_bot.py
+++ app/telegram_bot/telegram_bot.py
@@ -1,39 +1,33 @@
 import logging
 
 from asgiref.sync import sync_to_async
-from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove, BotCommand, Bot
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove, BotCommand, BotCommand
 from telegram.ext import Application, CommandHandler, ContextTypes, filters, ConversationHandler, MessageHandler
 
 from app.settings import TELEGRAM_BOT_TOKEN
-from app.telegram_bot.models import TgUser
 from app.telegram_bot.handlers import setup_handlers
+from app.telegram_bot.models import TgUser
 
-logger = logging.getLogger('django')
+logger = logging.getLogger('djabgo')
 
 COMMANDS = [
     BotCommand('start', 'Запустить бота'),
     BotCommand('subscribe', 'Подписаться на рассылку'),
-    BotCommand('subscribe_to_vacancies', 'Подписаться на вакансии'),
-    BotCommand('my_subscribes', 'Мои подписки'),
+    BotCommand('show_settings', 'Мои настройки фильтра'),
     BotCommand('settings', 'Настроить фильтры'),
-    BotCommand('loads', 'Выгрузить вакансии'),
 ]
 
 async def set_commands(application):
     await application.bot.set_my_commands(COMMANDS)
 
-# Commands
-
 def run_bot():
-    '''Main function for start telegram bot'''
-    logger.info('starting managment command')
+    logger.info('Starting telegram bot')
     
     
     application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
     
     
     setup_handlers(application)
-    # Устанавливаем команды
     application.post_init = set_commands
 
     logger.info('running bot')
