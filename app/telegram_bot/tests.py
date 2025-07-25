70c19bcb08ffc90b4780200ab9af6bf3ae96fb55
Marwin <gorn89@inbox.ru>
2025-07-25 18:34:49 +0400

    feat: add tests to bot handlers

--- app/telegram_bot/tests.py
+++ app/telegram_bot/tests.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
 from asgiref.sync import sync_to_async
 from unittest.mock import MagicMock
-from telegram import Update, Message, Chat, User
+from telegram import Update, Message, Chat, User, ReplyKeyboardRemove
+import unittest.mock
 
 from app.telegram_bot.models import TgUser
 from app.telegram_bot.handlers import start, subscribe
-from app.telegram_bot.statate_machine import settings, show_settings
+from app.telegram_bot.statate_machine import settings, show_settings, delete_settings, confirm_delete, select_profession, select_frontend_stack, select_backend_stack, finish_selection, cancel_selection, back_to_previose_stage, done, exit
 from app.telegram_bot.keyboards import (
     markup_filters, markup_front, markup_backend,
     markup_settings, markup_interval
@@ -46,4 +47,32 @@ class TestCommadHandlers(TestCase):
 
         calls = self.settings_message.reply_text.mock_calls
         self.assertEqual(calls[0].args[0], "Тут вы можете настроить свои фильтры")
+
+    async def test_show_settings(self):
+        # Без настроек
+        await show_settings(self.update, self.context)
+        self.message.reply_text.assert_called_with('У вас еще нет натроенного фильтра вакансий')
+
+    async def test_delete_settings_no_subscription(self):
+        
+        await delete_settings(self.update, self.context)
+        self.message.reply_text.assert_called_with("Нет сохранённых настроек для удаления.", reply_markup=markup_settings)
+
+    async def test_cancel_selection(self):
+        self.context.user_data = {'step': 'frontend'}
+        await cancel_selection(self.update, self.context)
+        self.assertEqual(self.context.user_data, {})
+        self.message.reply_text.assert_called_with("Вы сбросили настройки.", reply_markup=markup_settings)
+
+    async def test_finish_selection(self):
+        self.context.user_data = {'step': 'frontend', 'frontend_stack': {'React', 'Vue.js'}}
+        await finish_selection(self.update, self.context)
+        self.message.reply_text.assert_called_with("Выберите интервал получения уведомлений:", reply_markup=markup_interval)
+        self.assertEqual(self.context.user_data['step'], 'interval')
+
+    async def test_exit(self):
+        self.context.user_data = {'step': 'settings'}
+        await exit(self.update, self.context)
+        self.message.reply_text.assert_called_with("Выход из настроек фильтра", reply_markup=unittest.mock.ANY)
+        self.assertEqual(self.context.user_data, {})
 
