1be0776f92aa040b91927e29a67256ab607ced1a
Marwin <gorn89@inbox.ru>
2025-07-29 18:48:10 +0400

    fix: remove description

--- app/services/sender_service/sender.py
+++ app/services/sender_service/sender.py
@@ -8,10 +8,6 @@ from app.settings import TELEGRAM_BOT_TOKEN
 
 
 def send_email_message(to_email, subject, message):
-    """
-    Отправить email через стандартный Django backend.
-    Требует настройки EMAIL_HOST и других параметров в settings.py.
-    """
     send_mail(
         subject,
         message,
@@ -22,10 +18,6 @@ def send_email_message(to_email, subject, message):
 
 
 def send_telegram_message(chat_id, message):
-    """
-    Отправить сообщение через Telegram-бота.
-    Требует TELEGRAM_BOT_TOKEN в settings.py или переменных окружения.
-    """
     token = TELEGRAM_BOT_TOKEN
     bot = Bot(token=token)
     asyncio.run(bot.send_message(chat_id=chat_id, text=message))
